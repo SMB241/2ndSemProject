832d3d61bc7fa49d3d4a5f7d858b73e12c59df19
SMB241 <Mattbuen2805@gmail.com>
2025-03-27 08:40:52 +0800

    Add files via upload

--- AccountsReceivable.java
+++ AccountsReceivable.java
@@ -10,13 +10,15 @@ public class AccountsReceivable {
      String purchasedate;
      String customername;
     double invoiceamount;
+    String option;
     
     public void main(String[] args){
 
-        Payment customer1 = new Payment(invoicenum, purchasedate, customername, invoiceamount, invoiceamount,  paymentdate, paymentMethod);
+        Payment customer1 = new Payment(invoicenum, purchasedate, customername, invoiceamount, invoiceamount,  paymentdate, paymentMethod, option);
 
         customer1.createInvoice();
         customer1.AmountPaid();
+        customer1.loop();
     }
 
 }
--- Payment.java
+++ Payment.java
@@ -6,15 +6,18 @@ public class Payment extends Invoice{
    
 double amountpaid;
 String paymentdate;
+String option;
 int paymentMethod;
-    public Payment(String invoicenum, String purchasedate, String customername, double invoiceamount, double amountpaid, String paymentdate, int paymentMethod) {
+    public Payment(String invoicenum, String purchasedate, String customername, double invoiceamount, double amountpaid, String paymentdate, int paymentMethod,String option) {
             super(invoicenum, purchasedate, customername, invoiceamount);
             this.amountpaid = amountpaid;
             this.paymentdate =  paymentdate;
             this.paymentMethod = paymentMethod;
+            this.option = option;
         }
     
         Scanner s = new Scanner(System.in);
+        Scanner L = new Scanner(System.in);
 
     public void createInvoice(){
         System.out.println("Enter Invoice Number: ");
@@ -36,12 +39,12 @@ int paymentMethod;
     }
     public void AmountPaid(){
     double remainingBalance;
-    
-        System.out.println("Amount Paid: ");
-        amountpaid = s.nextDouble();
+        
+            System.out.println("Amount Paid: ");
+            amountpaid = s.nextDouble();
 
         System.out.println("Payment Date(YYY-MM-DD): ");
-        paymentdate = s.nextLine();
+        paymentdate = L.nextLine();
 
         System.out.println("Payment Method (1 for Cash, 2 for Check): ");
         paymentMethod = s.nextInt();
@@ -56,10 +59,39 @@ int paymentMethod;
         System.out.println("Payment Date: " + paymentdate);
         System.out.println("Payment Method: Cash");
             if(remainingBalance == 0){
-                System.out.println("Status: Fully Paid" );
+                System.out.println("Payment Status: Fully Paid" );
             }else{
                 System.out.println("Remaining Balance: " + remainingBalance);
             }
+         }else if(paymentMethod == 2){
+            remainingBalance =  invoiceamount - amountpaid;
+            System.out.println("Is the check on-date or post-date(O for On-date, P for Post-date): ");
+            String type = L.nextLine();
+            System.out.println("Enter Check Number: ");
+            int num = s.nextInt();
+            System.out.println("");
+            System.out.println("Payment Recorded Successfully!");
+            System.out.println("");
+            super.DisplayInfo();
+            System.out.println("Amount Paid: " + amountpaid);
+            System.out.println("Payment Date: " + paymentdate);
+            System.out.println("Payment Method: Check");
+            System.out.println("Check Type: On-date");
+            System.out.println("Check Number: " + num);
+            
+            if(remainingBalance == 0){
+                System.out.println("Payment Status: Fully Paid" );
+            }else{
+                System.out.println("Remaining Balance: " + remainingBalance);
+            }
+
+             }
+        
          }
-    }
-}
+     public void loop(){
+        System.out.println("Do you want to apply another payment to this invoice?: (Y/N)");
+        option = L.nextLine();
+        do AmountPaid(); while (option == "Y");
+            
+     }
+ }
